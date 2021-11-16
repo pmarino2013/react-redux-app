a3ca63566b3a74833a75c61919e8a5ac6ddff297
Pablo Marino <pmarino2013@gmail.com>
2021-11-16 15:23:12 -0300

    Template del proyecto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
